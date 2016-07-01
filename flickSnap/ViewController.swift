904bd07d40cbddca7c1a55734fc56c4956066bce
stanchiang <stanchiang23@gmail.com>
2016-06-30 22:14:14 -0700

    need to fix camera; then connect motion detection with taking a photo

--- flickSnap/ViewController.swift
+++ flickSnap/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreMotion
 import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -17,11 +18,42 @@ class ViewController: UIViewController {
     var magnitudeLabel:UILabel = UILabel()
     var initialAttitude:CMAttitude?
     
+    let captureSession = AVCaptureSession()
+    let stillImageOutput = AVCaptureStillImageOutput()
+    var error: NSError?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.whiteColor()
-
+        
+        let devices = AVCaptureDevice.devices().filter{ $0.hasMediaType(AVMediaTypeVideo) && $0.position == AVCaptureDevicePosition.Front }
+        if let captureDevice = devices.first as? AVCaptureDevice  {
+            
+//            captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &error))
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice)
+                captureSession.addInput(input)
+            } catch _ {
+                print("error: \(error?.localizedDescription)")
+            }
+            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+            captureSession.startRunning()
+            stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+            if captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addOutput(stillImageOutput)
+            }
+            if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+                previewLayer.bounds = view.bounds
+                previewLayer.position = CGPointMake(view.bounds.midX, view.bounds.midY)
+                previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                let cameraPreview = UIView(frame: CGRectMake(0.0, 0.0, view.bounds.size.width, view.bounds.size.height))
+                cameraPreview.layer.addSublayer(previewLayer)
+//                cameraPreview.addGestureRecognizer(UITapGestureRecognizer(target: self, action:"saveToCamera:"))
+                view.addSubview(cameraPreview)
+            }
+        }
+        
         view.addSubview(angleLabel)
         angleLabel.translatesAutoresizingMaskIntoConstraints = false
         angleLabel.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
@@ -39,7 +71,17 @@ class ViewController: UIViewController {
         processMotion()
         
     }
-
+    
+    func saveToCamera() {
+        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
+            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
+                (imageDataSampleBuffer, error) -> Void in
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
+            }
+        }
+    }
+    
     func processMotion(){
         motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.currentQueue()!) { (deviceMotion, error) in
             if let gravity = deviceMotion?.gravity {
@@ -59,7 +101,11 @@ class ViewController: UIViewController {
                     print("ready for capture")
                 } else {
                     self.view.backgroundColor = UIColor.whiteColor()
-                    print("capture")
+                    if (self.captureSession.running) {
+                        self.saveToCamera()
+                        print("capture")
+                        //Custom capture method.
+                    }
                 }
             }
         }
